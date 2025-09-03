831bf1c7d779a11e3685df3c0662aecddd1b4b1b
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-03 17:07:43 +0200

    esercizio 4 svolto

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,10 +5,27 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
-function countVowels()
+function countVowels(string) {
+let vowelCount = 0;
+const vowels = "aeiouAEIOU";
+
+for (let i = 0; i < string.length; i++) {
+
+const character = string[i];
+
+ // Se l'indice del carattere nella stringa 'vowels' è diverso da -1,
+    // significa che il carattere è una vocale.
+    if (vowels.indexOf(character) !== -1) { 
+      vowelCount++;
+    }
+  }
+
+return vowelCount;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = countVowels(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
